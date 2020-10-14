3d3b84e77ac87df9babc0b1acc34ec3448d71174
Jonasz Makulak <j.makulak@gmail.com>
2020-10-14 19:05:34 +0200

    Containers 2,3,4 refactoring

--- src/app/core/container-base/container-base.component.ts
+++ src/app/core/container-base/container-base.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container-base',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerBaseComponent implements OnInit {
 
+  @Input() title: string;
+  @Input() titleColor: string;
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
