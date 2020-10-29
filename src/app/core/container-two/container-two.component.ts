1c046b8e439d39b58e8920f9771fd23638e3491b
Jonasz Makulak <j.makulak@gmail.com>
2020-10-29 19:29:11 +0100

    changed second container

--- src/app/core/container-two/container-two.component.ts
+++ src/app/core/container-two/container-two.component.ts
@@ -25,6 +25,23 @@ export class ContainerTwoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.getElementById('programmerTab').click();
+  }
+
+  changeCard(sectionId: string) {
+    const elements: HTMLCollectionOf<Element> = document.getElementsByClassName('description');
+    const tabs: HTMLCollectionOf<Element> = document.getElementsByClassName('tab');
+
+    for (let i = 0; i < elements.length; i++) {
+      (elements[i] as HTMLElement).style.display = 'none';
+    }
+
+    for (let i = 0; i < tabs.length; i++) {
+      tabs[i].className = tabs[i].className.replace('active', '');
+    }
+
+    document.getElementById(sectionId).style.display = 'block';
+    document.getElementById(sectionId + 'Tab').className += ' active';
   }
 
 }
